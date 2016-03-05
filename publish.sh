ef2baa390e125a6f5b859e231db6e0c4c05836cd
richardkdrew <richardkdrew@users.noreply.github.com>
2016-03-06 12:14:46 +1300

    Updated AWS publish script to exclude bower_components

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 BUCKET=rd-photography-$CIRCLE_BRANCH
-aws s3 sync dist/public/ s3://$BUCKET/ --exclude "dist/public/bower_components/*"
+cd dist/public
+aws s3 sync . s3://$BUCKET/ --exclude "bower_components/*"
