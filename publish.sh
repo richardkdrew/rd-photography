09bed74e7aface638d21b51422d621f9b242f2df
richardkdrew <richardkdrew@users.noreply.github.com>
2016-03-06 15:32:36 +1300

    Updated AWS publish script to set cache control header

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 BUCKET=rd-photography-$CIRCLE_BRANCH
 cd dist/public
-aws s3 sync . s3://$BUCKET/ --exclude "bower_components/*"
+aws s3 sync . s3://$BUCKET/ --exclude "bower_components/*" --recursive --cache-control max-age=604800
