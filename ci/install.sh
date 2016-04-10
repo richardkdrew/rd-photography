931282bd44ed21ac22edea0cefd25332e2a3946f
richardkdrew <richardkdrew@users.noreply.github.com>
2016-04-10 20:43:11 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/install.sh
+++ ci/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 npm set progress=false
 
-cd photography
+cd ./photography
 npm install --silent
 
 npm install -g bower
-cd photography/client
-bower install ../bower.json --allow-root --silent
-cd ./
+cd ./photography/client
+bower install ./photography/bower.json --allow-root --silent
 
+cd ./
 cp -r ./photography/* ./photography_after_dependency_install
