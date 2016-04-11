addcbf3cf4b267fa76f79561de096c43e006ce12
richardkdrew <richardkdrew@users.noreply.github.com>
2016-04-11 19:04:31 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 npm set progress=false
 npm install -g grunt-cli --silent
+
 cd photography
 grunt build
+
+cd ..
+cp -r photography/dist/public* build_artifacts
+rm -rf build_artifacts/bower_components
