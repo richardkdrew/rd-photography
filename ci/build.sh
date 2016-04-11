694d4cacca72d8be2f94f166db8b544aae6b32a9
richardkdrew <richardkdrew@users.noreply.github.com>
2016-04-11 20:05:44 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/build.sh
+++ ci/build.sh
@@ -6,5 +6,5 @@ cd photography
 grunt build
 
 cd ..
-VERSION=cat version/number
-tar -zcvf build_artifacts/photography-{$VERSION}.tar.gz --exclude='bower_components' photography/dist/public/*
+VERSION=$(cat version/number)
+tar -zcvf build_artifacts/photography-$VERSION.tar.gz --exclude='bower_components' photography/dist/public/*
