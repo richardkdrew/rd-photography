9fb7ce7bb786e55475125ae908078fe8743474fa
Richard Drew <richardkdrew@users.noreply.github.com>
2016-04-19 15:51:00 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/build.sh
+++ ci/build.sh
@@ -10,5 +10,5 @@ VERSION=$(cat version/number)
 cd photography/dist/public
 tar -cvzf photography-$VERSION.tar.gz --exclude='bower_components' . -v
 
-cd ../..
-cp photography/dist/public/photography-$VERSION.tar.gz .
+cd ../../..
+cp photography/dist/public/photography-$VERSION.tar.gz build_artifacts
