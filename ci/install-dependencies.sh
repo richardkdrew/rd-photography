cfa291f4097211d7b6aee2b4f1db46cc00e32406
Richard Drew <richardkdrew@users.noreply.github.com>
2016-04-22 14:56:52 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -11,4 +11,4 @@ npm install --silent
 bower install --allow-root --silent
 
 # making dependencies available as an output
-cp -r ../photography/. photography_after_dependency_install
+cp -r . ../photography_after_dependency_install
