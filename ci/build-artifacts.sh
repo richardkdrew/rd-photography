03e9bd2faa736f71d9f73ca2ec5e2737f8b4d3aa
Richard Drew <richardkdrew@users.noreply.github.com>
2016-04-22 17:38:13 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/build-artifacts.sh
+++ ci/build-artifacts.sh
@@ -10,5 +10,7 @@ cd photography
 grunt build
 
 # creating build artifacts archive and making build artifacts available as an output
-cd dist/public
-tar -cvzf ../../../build_artifacts/photography-$VERSION.tar.gz --exclude='bower_components' * -v
+#cd dist/public
+#tar -cvzf ../../../build_artifacts/photography-$VERSION.tar.gz --exclude='bower_components' * -v
+cp photography/dist/public/* build_artifacts
+rm -rf build_artifacts/bower_components
