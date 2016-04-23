021bcfa4d14318ef807564a72e2a5baaf3554bb7
richardkdrew <richardkdrew@users.noreply.github.com>
2016-04-23 17:30:33 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/create-deployment.sh
+++ ci/create-deployment.sh
@@ -6,3 +6,6 @@ VERSION="v$(cat version/number)"
 # preparing deployment files
 cd deployment
 tar -xvzf ../photography-rc/photography-$VERSION.tgz
+
+cd ..
+echo deployment/* > deployment/photography.include
