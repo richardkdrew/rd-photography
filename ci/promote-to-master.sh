c4824f1e1cad107062497e6a47532828a40a926b
Richard Drew <richardkdrew@users.noreply.github.com>
2016-04-29 15:41:15 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/promote-to-master.sh
+++ ci/promote-to-master.sh
@@ -2,4 +2,7 @@
 
 set -e -x
 
-echo "Hello World!"
+git pull --ff-only         # check we are up to date on our branch
+git checkout master        # go the target branch
+git pull --ff-only         # check updates there too
+git merge develop --no-ff  # merge with a commit
