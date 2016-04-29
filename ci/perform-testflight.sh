1ce005d04ff02c940929439652a31287287f85b4
richardkdrew <richardkdrew@users.noreply.github.com>
2016-04-29 18:19:49 +1200

    Experimenting with split dependency installs for ci pipeline

--- ci/perform-testflight.sh
+++ ci/perform-testflight.sh
@@ -2,4 +2,13 @@
 
 set -e -x
 
-echo "Hello World!"
+# set up the app
+mkdir app
+cd app
+tar -xvzf ../candidate-release/photography-$VERSION.tgz
+
+# copy the app files into pace
+cp . /usr/share/nginx/html
+
+# placeholder for testing app
+curl http://127.0.0.1/index.html
