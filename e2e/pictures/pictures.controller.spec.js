4ec40d08e6dff60ac550d4c72033e98ae264b75b
Richard Drew <richardkdrew@users.noreply.github.com>
2016-05-02 17:13:01 +1200

    Experimenting with split dependency installs for ci pipeline

---
+++ e2e/pictures/pictures.controller.spec.js
@@ -0,0 +1,15 @@
+//
+// test/e2e/pictures/pictures.controller.spec.js
+//
+describe("Pictures Controller", function() {
+
+  beforeEach(function() {
+    browser.get('/');
+  });
+
+  it('should automatically redirect to / when location is pictures', function() {
+    browser.get('/pictures');
+    expect(browser.getLocationAbsUrl()).toMatch("/");
+  });
+
+});
